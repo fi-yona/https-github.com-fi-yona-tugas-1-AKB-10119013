38ae822142f29cb4892046cc3b002b2640276ee3
fi-yona <fionaavila15@gmail.com>
2022-04-22 15:55:06 +0700

    masih bug

--- app/src/main/java/com/example/tugas1akb_10119013/ScreenRegistrasiActivity.java
+++ app/src/main/java/com/example/tugas1akb_10119013/ScreenRegistrasiActivity.java
@@ -6,10 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ScreenRegistrasiActivity extends AppCompatActivity {
 
-    private Button btn_kembali_login;
+    private Button btn_kembali_login, btn_daftarkan;
+    private EditText input_username_regis, input_password_regis;
+    private String username_regis, password_regis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +28,31 @@ public class ScreenRegistrasiActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent ikl = new Intent(ScreenRegistrasiActivity.this, Main2Activity.class);
                 startActivity(ikl);
+            }
+        });
+
+        initView();
+        setData();
+    }
+
+    public void initView(){
+        btn_daftarkan = findViewById(R.id.btn_daftarkan);
+        input_username_regis = findViewById(R.id.input_username_regis);
+        input_password_regis = findViewById(R.id.input_password_regis);
+    }
+
+    public void setData(){
+        btn_daftarkan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                username_regis = input_username_regis.getText().toString();
+                password_regis = input_password_regis.getText().toString();
+
+                Intent intent = new Intent(ScreenRegistrasiActivity.this, Main2Activity.class);
+                intent.putExtra("username_regis", username_regis);
+                intent.putExtra("password_regis", password_regis);
+                startActivity(intent);
+                Toast.makeText(ScreenRegistrasiActivity.this, "berhasil didaftarkan!", Toast.LENGTH_SHORT).show();
             }
         });
     }
